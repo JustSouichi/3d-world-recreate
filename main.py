233821d6f5abb58ef2d38b94e71176fef4e71bc6
TommasoBertocchiDeveloper <tommasobertocchideveloper04@gmail.com>
2023-11-16 22:08:13 +0100

    *

--- main.py
+++ main.py
@@ -2,6 +2,13 @@ import sys
 import os
 import cv2
 import numpy as np
+import pygame
+from pygame.locals import *
+
+
+
+W = 1920  
+H = 1080 
 
 def add_circles_to_edges(frame):
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -18,6 +25,10 @@ def add_circles_to_edges(frame):
 try:
   video_path = sys.argv[1]
   video_path = "drive.mp4"
+  pygame.init()
+  display_pygame = (W, H)
+  pygame.display.set_mode(display_pygame)
+  pygame.display.set_caption("Pygame window")
   if os.path.exists(video_path):
      cap = cv2.VideoCapture(video_path)
      if (cap.isOpened() == False):
@@ -25,20 +36,45 @@ try:
      while (cap.isOpened()):
         ret, frame = cap.read()
         if ret == True:
-          W = 700
-          H = 400
           add_circles_to_edges(frame)
           cv2.namedWindow('Frame', cv2.WINDOW_NORMAL)
           cv2.resizeWindow('Frame', (W, H))
           cv2.imshow('Frame', frame)
           print(frame)
+          pygame_screen = pygame.display.get_surface()
+          pygame_screen.fill((0, 0, 0))
+          circles = []
+          gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+          blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+          edges = cv2.Canny(blurred, 50, 150)
+          contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+          for contour in contours:
+            if cv2.contourArea(contour) > 0.0:
+              (x, y), radius = cv2.minEnclosingCircle(contour)
+              center = (int(x), int(y))
+              radius = 1
+              circles.append((center[0], center[1], radius))
+          pygame_screen = pygame.display.get_surface()
+          pygame.fill = ((0, 0, 0))
+          for circle in circles:
+            x, y, radius = circle
+            pygame.draw.circle(pygame_screen, (0, 255, 0), (x, y), radius, 2)
+          pygame.display.flip()
           if cv2.waitKey(25) & 0xFF == ord('q'):
             break
+          for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                cap.release()
+                pygame.quit()
+                cv2.detroyAllWindows()
+                quit()
         else:
           break
      cap.release()
      cv2.destroyAllWindows()
   else:
     print("error: incorrent video path")
+except Exception as e:
+  print(e)
 except:
  print("error: no path video insert")
